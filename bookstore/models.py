8c97ac178b6984057d38d69d85bb3f423aa00113
Chris Muthig <camuthig@gmail.com>
2020-05-02 10:39:13 -0700

    Set up basic bookstore example and models

---
+++ bookstore/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=256)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=256)
