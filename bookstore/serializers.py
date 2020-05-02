0e180eb8703fa9b47f05a4363e2edfcf345def12
Chris Muthig <camuthig@gmail.com>
2020-05-02 11:44:05 -0700

    Implement a dummy hashed ID pattern

--- bookstore/serializers.py
+++ bookstore/serializers.py
@@ -1,5 +1,24 @@
+import base64
+
 from rest_framework import serializers
 from bookstore import models
+
+
+class HashedPrimaryKeyRelatedField(serializers.PrimaryKeyRelatedField):
+    def to_internal_value(self, data):
+        # TODO This could be implemented in a safe manner
+        data = base64.urlsafe_b64decode(data).decode('utf-8')
+        return super().to_internal_value(data)
+
+    def to_representation(self, value):
+        # TODO Research how to use pk_field
+        if value is None:
+            return value
+
+        id_value = f'{value.pk}'
+
+        hashed = base64.urlsafe_b64encode(bytes(id_value, 'utf-8')).decode('utf-8')
+        return hashed
 
 
 class AuthorSerializer(serializers.ModelSerializer):
@@ -12,3 +31,5 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Book
         fields = ['title', 'author']
+
+    serializer_related_field = HashedPrimaryKeyRelatedField
